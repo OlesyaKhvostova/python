fea920ae580e7a5478c92b5ddf20a0bf292ab7b4
olesya <olesya_khvostova@mail.ru>
2023-01-15 22:35:44 +0300

    Add Bot Downloader video and audio

--- Homework9/BaseBot.py
+++ Homework9/BaseBot.py
@@ -3,48 +3,95 @@ import os
 import DownloderBot
 api_token = ''
 bot = telebot.TeleBot(api_token)
+bot.disable_web_page_preview = True
 commands_list = ['/start','/downloadvideo','/downloadmp3','/enterpath']
 
-def load_path_from_file():
-    main_path = ''
-    with open('downloder_bot_settings.txt', 'r') as f:
-        main_path = f.readline()
+#def load_path_from_file():
+ #   main_path = ''
+ #   with open('downloder_bot_settings.txt', 'r') as f:
+  #      main_path = f.readline()
 
-    return main_path
+ #   return main_path
 
-def write_path_from_file(main_path):
-    with open('downloder_bot_settings.txt', 'w') as f:
-        f.write(main_path)
+#def write_path_from_file(main_path):
+ #   with open('downloder_bot_settings.txt', 'w') as f:
+ #       f.write(main_path)
 
 #@bot.message_handler(content_types=['text'])
 #def start(message):
-    #if message.text != '/start':
-        #bot.send_message(message.chat.id, text=f"Напишите команду '/start'")
+    #bot.send_message(message.chat.id, text=f"Напишите команду '/start'")
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "/path - задать путь\n/loadvideo - скачать видео\n/loadmusic - скачать музыку")
+    bot.send_message(message.chat.id, "/loadvideo - скачать видео\n/loadmusic - скачать музыку")
 
-@bot.message_handler(commands=['path'])
-def path_message(message):
-    main_path = message.text
-    directory = os.listdir(os.curdir)
-    print(directory)
-    print(f'Main path = {main_path}')
-    write_path_from_file(main_path)
+#@bot.message_handler(commands=['path'])
+#def path_message(message):
+ #   main_path = message.text
+ #   directory = os.listdir(os.curdir)
+  #  print(directory)
+   # print(f'Main path = {main_path}')
+   # write_path_from_file(main_path)
 
 @bot.message_handler(commands=['loadvideo'])
 def loadvideo_message(message):
-    main_path = load_path_from_file()
-    os.mkdir(main_path)
-    DownloderBot.Download(message.text, main_path, 1)
-    bot.send_message(message.chat.id,'Видео скачено')
+    #main_path = 'Bot\Video'#load_path_from_file()
+    #print(main_path)
+    #if not os.path.exists(main_path):
+    #    os.mkdir(main_path)
+    bot.send_message(message.chat.id, 'Введите ссылку на видео')
+    bot.register_next_step_handler(message, enter_video_link)
+    #output_filename = DownloderBot.Download(message.text, main_path, 1)
+    #print(message.text)
+    #bot.send_video(message.chat.id, video=open(output_filename, 'rb'), supports_streaming=True)
+    #bot.send_message(message.chat.id,'Видео скачено')
+
+def enter_video_link(message):
+    create_base_folder()
+    bot.send_message(message.chat.id, 'Скачеваем видео')
+    output_filename = DownloderBot.Download(message.text, main_path, 1)
+    print(message.text)
+    if output_filename:
+        bot.send_video(message.chat.id, video=open(output_filename, 'rb'), supports_streaming=True)
+        bot.send_message(message.chat.id, 'Видео скачено')
+        delete_media_file(output_filename)
+    else:
+        bot.send_message(message.chat.id, 'Ошибка: слишком большое видео')
 
 @bot.message_handler(commands=['loadmusic'])
 def loadmusic_message(message):
-        main_path = load_path_from_file()
+    bot.send_message(message.chat.id, 'Введите ссылку на музыку')
+    bot.register_next_step_handler(message, enter_music_link)
+        #main_path = 'Bot'#load_path_from_file()
+        #print(main_path)
+        #if not os.path.exists(main_path):
+        #    os.mkdir(main_path)
+        #print(message.text)
+        #bot.send_message(message.chat.id, 'Скачеваем музыку')
+        #output_filename = DownloderBot.Download(message.text, main_path, 0)
+        #bot.send_audio(message.chat.id, output_filename)
+        #bot.send_message(message.chat.id, 'Музика скачена')
+
+def enter_music_link(message):
+    create_base_folder()
+    bot.send_message(message.chat.id, 'Скачеваем музыку')
+    output_filename = DownloderBot.Download(message.text, main_path, 0)
+    print(message.text)
+    if output_filename:
+        bot.send_audio(message.chat.id, audio=open(output_filename, 'rb'))
+        bot.send_message(message.chat.id, 'Музыка скачена')
+        delete_media_file(output_filename)
+    else:
+        bot.send_message(message.chat.id, 'Ошибка: слишком большое аудио')
+
+def create_base_folder():
+    main_path = '\Bot'
+    print(main_path)
+    if not os.path.exists(main_path):
         os.mkdir(main_path)
-        DownloderBot.Download(message.text, main_path, 0)
-        bot.send_message(message.chat.id, 'Музика скачена')
+
+def delete_media_file(output_file):
+    if os.path.exists(output_file):
+        os.remove(output_file)
 
 bot.polling(none_stop=True)
--- Homework9/DownloderBot.py
+++ Homework9/DownloderBot.py
@@ -1,28 +1,51 @@
 from pytube import YouTube
 import os
 
+MAX_FILE_SIZE = 52428800
+def check_size(filesize, youtubeObject):
+    media_object = None
+    if youtubeObject.streams.get_highest_resolution().filesize < MAX_FILE_SIZE:
+        media_object = youtubeObject.streams.get_highest_resolution()
+    elif youtubeObject.streams.get_lowest_resolution().filesize < MAX_FILE_SIZE:
+        media_object = outubeObject.streams.get_lowest_resolution()
+
+    return media_object
+
 def rename_file_to_mp3(output_filename):
     base, ext = os.path.splitext(output_filename)
     new_file = base + '.mp3'
+    if os.path.exists(new_file):
+        os.remove(new_file)
+
     os.rename(output_filename, new_file)
     print(new_file)
+    return new_file
 
 def Download(link, output_path,video):
     youtubeObject = YouTube(link)
+    filesize = youtubeObject.streams.get_highest_resolution().filesize
+
+
+    print(f'Size = {filesize}')
     if video:
-        youtubeObject = youtubeObject.streams.get_highest_resolution()
+        youtubeObject = check_size(filesize, youtubeObject)
     else:
-        youtubeObject = youtubeObject.streams.filter(only_audio=True).first()
+        youtubeObject = youtubeObject.streams.get_audio_only()
 
     output_filename = None
     try:
-        output_filename = youtubeObject.download(output_path)
+        if youtubeObject:
+            output_filename = youtubeObject.download(output_path)
+        else:
+            return output_filename
     except:
         print("Произошла ошибка")
 
     print(output_filename)
     if not video:
-        rename_file_to_mp3(output_filename)
+        output_filename = rename_file_to_mp3(output_filename)
+
+    return output_filename
 
 def run():
     output_path = input("Введите путь для сохранения:\n")
